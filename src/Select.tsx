55c962e45851a2e864ee9b1f616280c27eedd9fb
robggarcia <robert.g.garcia82@gmail.com>
2023-04-18 21:52:42 -0400

    css

--- src/Select.tsx
+++ src/Select.tsx
@@ -3,7 +3,7 @@ import styles from "./select.module.css";
 
 type SelectOptions = {
   label: string;
-  value: string;
+  value: any;
 };
 
 type SelectProps = {
@@ -15,8 +15,19 @@ type SelectProps = {
 const Select = ({ value, onChange, options }: SelectProps) => {
   return (
     <>
-      <div className="container">Wrong</div>
-      <div className={styles.container}>Right</div>
+      <div tabIndex={0} className={styles.container}>
+        <span className={styles.value}>Value</span>
+        <button className={styles["clear-btn"]}>&times;</button>
+        <div className={styles.divider}></div>
+        <div className={styles.caret}></div>
+        <ul className={`${styles.options} ${styles.show}`}>
+          {options.map((option) => (
+            <li key={option.label} className={styles.option}>
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
